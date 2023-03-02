e61de9cb6d0b12fed7ee99ab02f59f2035229a0e
LeventOzcelik89 <Levent.ozcelik@outlook.com>
2023-03-02 11:45:27 +0300

    z

---
+++ src/Infrastructure/TaskManager.Persistence/ScaffoldingDesignTimeServices.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Persistence
+{
+    public class ScaffoldingDesignTimeServices : IDesignTimeServices
+    {
+        public void ConfigureDesignTimeServices(IServiceCollection services)
+        {
+            services.AddHandlebarsScaffolding();
+        }
+    }
+
+}
